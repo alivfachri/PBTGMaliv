3c77f9b4e303a2d33286fc411ae096bd1a6a3399
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-27 13:47:23 +0700

    OPERATOR PADA JAVA

---
+++ Pertemuan4.Tugas2/src/main/java/com/mycompany/pertemuan4/tugas2/Pertemuan4Tugas2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4.tugas2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan4Tugas2 {
+
+    public static void main(String[] args) {
+int w = 5;
+int x = 6;
+int y = 8 - x++;
+int z = 8 - ++w;
+
+System.out.println(" Nilai w = " + w);
+System.out.println(" Nilai x = " + x);
+System.out.println(" Nilai y = " + y);
+System.out.println(" Nilai z = " + z);
+    }
+}
