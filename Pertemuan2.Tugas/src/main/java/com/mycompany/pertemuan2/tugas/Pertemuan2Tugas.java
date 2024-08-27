7c27bca31f017dffb3e8ccfb7227c76fde3b03de
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-27 13:31:45 +0700

    VARIABLE PADA JAVA

---
+++ Pertemuan2.Tugas/src/main/java/com/mycompany/pertemuan2/tugas/Pertemuan2Tugas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2.tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan2Tugas {
+
+    public static String makanan;
+//Membuat variable tanpa Value/Nilai pada class staticvariable
+
+       public static final String minuman = "jus jambu";
+
+        public static void main(String[] args) {
+            
+        
+            // TODO code application logic here
+            makanan = "ayam geprek ";//Mengisi Value pada variable Hobi
+            System.out.println("makanan favorit saya : " + makanan);//Memanggil Variable makanan
+            System.out.println("minuman favorit saya Saya : "+ minuman );//Memanggil Variable hewan
+    }
+}
