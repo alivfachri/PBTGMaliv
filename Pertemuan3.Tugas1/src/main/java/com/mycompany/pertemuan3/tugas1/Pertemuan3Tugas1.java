e25c1f0833b0b0d6c2275ea812fdec5728d8cc0c
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-27 13:36:38 +0700

    TIPE DATA PADA JAVA

---
+++ Pertemuan3.Tugas1/src/main/java/com/mycompany/pertemuan3/tugas1/Pertemuan3Tugas1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3.tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan3Tugas1 {
+
+    public static void main(String[] args) {
+       short angka;
+
+        angka = 9;
+        System.out.println(angka);
+
+        float f1 = 8.99f;
+        System.out.println(f1);
+
+        char cha = 'A';
+        System.out.println(cha);
+
+        boolean b = false;
+        System.out.println(b);
+
+        System.out.println("Hello World!");
+    }
+}
