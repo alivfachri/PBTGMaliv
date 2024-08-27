b9efd6e044357074474f7af4c0599897e9df8517
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-27 12:55:01 +0700

    VARIABLE PADA JAVA

---
+++ Pertemuan1.tugas1/src/main/java/com/mycompany/pertemuan1/tugas1/Pertemuan1Tugas1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan1.tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan1Tugas1 {
+
+    public static void main(String[] args) {
+        int a = 50;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
