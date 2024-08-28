1c5ad9d2b4fb6132b607ed4155faa0ebba160d81
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-28 09:06:26 +0700

    MENGONTROL ALIRAN PROGRAM PERULANGAN WHILE, DO

---
+++ Pertemuan9.Tugas3/src/main/java/com/mycompany/pertemuan9/tugas3/Pertemuan9Tugas3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan9.tugas3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan9Tugas3 {
+
+    public static void main(String[] args) {
+          int i = 1;
+        while (i < 6) {
+          System.out.println(i);
+          i++;
+}
+    }
+}
