6edf42830537712984854e7ab4f2ce982fc2c330
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-28 09:08:23 +0700

    KONSEP PEMROGRAMAN BERORIENTASI OBJEK CLASS,

---
+++ Pertemuan10.Tugas1/src/main/java/com/mycompany/pertemuan10/tugas1/Pertemuan10Tugas1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan10.tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan10Tugas1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
