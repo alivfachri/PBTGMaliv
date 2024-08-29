0fbddd5f406bc2f90ca79d086879cf643b694f50
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-29 12:59:34 +0700

    ABSTRACT CLASS INTERFACE

---
+++ Pertemuan14.Tugas1/src/main/java/com/mycompany/pertemuan14/tugas1/KucingAbstrak.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan14.tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KucingAbstrak extends AbstrakHewan {
+    
+    @Override
+    public void suara() {
+        System.out.println(" Suara Kucig Mengeong");
+    }
+}
---
+++ Pertemuan14.Tugas1/src/main/java/com/mycompany/pertemuan14/tugas1/Pertemuan14Tugas1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan14.tugas1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan14Tugas1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AbstrakHewan objek = new KucingAbstrak();
+        objek.suara();
+        objek.suara2();
+    }
+}
