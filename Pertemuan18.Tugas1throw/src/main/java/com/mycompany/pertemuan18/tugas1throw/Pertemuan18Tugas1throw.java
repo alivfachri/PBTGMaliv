5257890b26f6a2808ea20208101fb6ecaeebc46a
alivfachri <145528857+alivfachri@users.noreply.github.com>
2024-08-30 20:26:23 +0700

    EXCEPTION HANDLING (REVISI THROW)

--- Pertemuan18.Tugas1throw/src/main/java/com/mycompany/pertemuan18/tugas1throw/Pertemuan18Tugas1throw.java
+++ Pertemuan18.Tugas1throw/src/main/java/com/mycompany/pertemuan18/tugas1throw/Pertemuan18Tugas1throw.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
 package com.mycompany.pertemuan18.tugas1throw;
 
 /**
@@ -9,23 +10,21 @@ package com.mycompany.pertemuan18.tugas1throw;
  */
 public class Pertemuan18Tugas1throw {
 
-    public static void validate(int age) {
+    public static void validate(int age){
         if (age < 17) {
-            // throw ArithmeticException jika tidak eligible
-            throw new ArithmeticException("Orang ini belum eligible untuk vote!!");
-        } else {
-            System.out.println("Orang ini sudah eligible untuk vote.");
+            //throw Arithmetic exception jika eligible
+            throw new ArithmeticException(
+            "Orang ini Belum eligible untuk vote!!");
+        }
+        else{
+            System.out.println(
+            "Orang ini Sudah eligible untuk vote");
         }
     }
-
-    // main method
-    public static void main(String[] args) {
-        // calling the function
-        try {
-            validate(13);
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Rest of the code...");
+    //main method
+    public static void main (String[] args) {
+        //calling the fuction
+        validate(13);
+        System.out.println("rest of the code...");
     }
 }
